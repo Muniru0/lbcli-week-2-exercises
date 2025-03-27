1974cd9d57b1ca8a4771c2119c2321667e7ff210
Yussif Muniru <yussifmuniruwulon@gmail.com>
2025-03-27 14:10:59 +0000

    Fix 8

--- submission/07.sh
+++ submission/07.sh
@@ -3,31 +3,30 @@
 # raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
 
-# 1) The two UTXOs (same TXID, different vout indices):
+
+# 1) Decode the raw transaction to extract the TXID
 PREV_TXID=$(bitcoin-cli -regtest decoderawtransaction "01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000" | jq -r '.txid')
+
+# 2) UTXO input indices
 VOUT_0=0
 VOUT_1=1
 
-# 2) Destination address (testnet P2SH). We send 0.2 BTC here.
+# 3) Destination address (testnet P2SH)
 DEST_ADDR="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-DEST_AMOUNT="0.20000000"  # 20,000,000 sat
+DEST_AMOUNT="0.20000000"  # 20,000,000 sats
 
-# 3) change address 
-CHANGE_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "SegWit Address" bech32)
-# Remaining 0.03678108 BTC after we pay 0.2 BTC + 0.00001000 BTC fee
-CHANGE_AMOUNT="0.03678108"
-
-# 4) Create the raw transaction with bitcoin-cli:
+# 4) Create the raw transaction using both inputs, no change output, and enable RBF
 unsigned_hex="$(
   bitcoin-cli -regtest -rpcwallet=btrustwallet createrawtransaction \
     "[
-       {\"txid\":\"$PREV_TXID\",\"vout\":$VOUT_0},
-       {\"txid\":\"$PREV_TXID\",\"vout\":$VOUT_1}
+       {\"txid\":\"$PREV_TXID\",\"vout\":$VOUT_0, \"sequence\":4294967293},
+       {\"txid\":\"$PREV_TXID\",\"vout\":$VOUT_1, \"sequence\":4294967293}
      ]" \
     "{
-       \"$DEST_ADDR\":$DEST_AMOUNT,
-       \"$CHANGE_ADDR\":$CHANGE_AMOUNT
+       \"$DEST_ADDR\": $DEST_AMOUNT
      }"
 )"
 
+# 5) Output the hex
+echo "Raw unsigned transaction:"
 echo "$unsigned_hex"
