c5376b2fa695a27ca6c790a2ab035201a6288c52
Yussif Muniru <yussifmuniruwulon@gmail.com>
2025-03-27 13:42:01 +0000

    Fix 2

--- submission/03.sh
+++ submission/03.sh
@@ -2,7 +2,7 @@
 addr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "SegWit Address" bech32)
 
 # Add funds to the address.
-bitcoin-cli -regtest -rpcwallet-btrustwallet generatetoaddress 100 "$addr"
+bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 100 "$addr"
 
 # Return only the Address
 echo "$addr"
